be6f014f277aa82d5518f8f7fa1ccc8b9d67f230
Hybola <keeratipong.b@gmail.com>
2023-04-25 13:26:08 +0700

    finish internal state

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -4,19 +4,29 @@ import React, { useState } from "react";
 export function TodoForm({ onSetIsAddMode }) {
   // # 1 : Logic Section
   const [task, setTask] = useState("");
+  const [isError, setIsError] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Click Add Task button");
+    // console.log("Click Add Task button");
     //จบ AddMode
+
+    // validate
+    if (task.trim() === "") {
+      setIsError(true);
+      return;
+    }
+
+    // set back to normal mode
     onSetIsAddMode(false);
   };
   const handleClickCancel = (e) => {
-    console.log("Click Cancel button");
+    // console.log("Click Cancel button");
     onSetIsAddMode(false);
   };
 
   const handleChangeInput = (e) => {
-    console.log(e.target.value);
+    setIsError(false);
+    // console.log(e.target.value);
     setTask(e.target.value);
   };
   // # 2 : UI-Section
@@ -29,7 +39,7 @@ export function TodoForm({ onSetIsAddMode }) {
         onChange={handleChangeInput}
       />
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}>Title is required</p>
+        {isError && <p className={styles.todo__error}>Title is required</p>}
         <div className={styles.todo__form__buttons}>
           <button type="button" onClick={handleClickCancel}>
             Cancel
